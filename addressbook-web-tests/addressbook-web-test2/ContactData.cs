63d1ab3d298f3416796992b2bb3b37c77b31ee46
User <User@USER-PC>
2021-06-29 19:43:25 +0400

    Создала второй проект с типом Проект модульного теста

---
+++ addressbook-web-tests/addressbook-web-test2/ContactData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename = "";
+        private string nickname = "";
+       // private byte photo;
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private int telephone;
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string Email = "";
+        private string Email2 = "";
+        private string Email3 = "";
+        private string homepage = "";
+      //  private DateTime birthday;
+      //  private DateTime anniversary;
+      //  private List<GroupData> group;
+        private string secondaryaddress = "";
+        private string secondaryhome = "";
+        private string secondarynotes = "";
+
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname= value;
+            }
+        }
+       
+    }
+}
+
