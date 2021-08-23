e6bf4b405aeb7d05cec99224c844b29619d632b7
User <User@USER-PC>
2021-08-23 20:10:42 +0400

    Лекция 5.5

--- addressbook-web-tests/addressbook-web-test2/Tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-test2/Tests/GroupCreationTests.cs
@@ -10,32 +10,25 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        [Test]
-        public void GroupCreationTest()
+
+        public static IEnumerable<GroupData> RandomGroupDataProvider()
         {
-            GroupData group = new GroupData("group 2");
-            group.Header = "group 2";
-            group.Footer = "group 2";
-
-            List<GroupData> oldGroups = app.Groups.GetGroupList();
-
-            app.Groups.Create(group);
-
-            Assert.AreEqual(oldGroups.Count + 1, app.Groups.GetGroupCount());
-
-            List<GroupData> newGroups = app.Groups.GetGroupList();
-            oldGroups.Add(group);
-            oldGroups.Sort();
-            newGroups.Sort();
-            Assert.AreEqual(oldGroups, newGroups);
+            List<GroupData> groups = new List<GroupData>();
+            for (int i = 0; i < 5; i++)
+            {
+                groups.Add(new GroupData(GenerateRandomString(30))
+                {
+                    Header = GenerateRandomString(100),
+                    Footer = GenerateRandomString(100)
+                });
+            }
+            return groups;
         }
-        [Test]
-        public void EmptyGroupCreationTest()
-        {
 
-            GroupData group = new GroupData("");
-            group.Header = "";
-            group.Footer = "";
+
+        [Test, TestCaseSource ("RandomGroupDataProvider")]
+        public void GroupCreationTest(GroupData group)
+        {
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
 
